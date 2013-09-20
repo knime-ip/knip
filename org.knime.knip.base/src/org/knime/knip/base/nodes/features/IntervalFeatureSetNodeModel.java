knime-ip/knip

commit 88213ee6ee48c7b114ed1396e4112004e7e5f4e4

Revised Shape Descriptors Feature Set: - FDComplexCentralDistance removed - Approximated distances (polar images) replaced by calculating the distance on the orginal contour points determined by tracing the contour - option to specify the number of bins removed, as it probably doesn't makes sense (?)

org.knime.knip.base/src/org/knime/knip/base/nodes/features/providers/ShapeDescriptorFeatureSetProvider.java
@@ -69,9 +69,8 @@ import org.knime.core.node.defaultnodesettings.SettingsModelInteger;
 import org.knime.core.node.defaultnodesettings.SettingsModelStringArray;
 import org.knime.knip.core.features.FeatureFactory;
 import org.knime.knip.core.features.FeatureSet;
-import org.knime.knip.core.features.fd.CentralDistanceFeatureSet;
-import org.knime.knip.core.features.fd.FDCentralDistanceFeatureSet;
-import org.knime.knip.core.features.fd.FDComplexCoordinatesFeatureSet;
+import org.knime.knip.core.features.fd.CentroidDistanceFeatureSet;
+import org.knime.knip.core.features.fd.FDCentroidDistanceFeatureSet;
 import org.knime.knip.core.util.EnumListProvider;
 
 /**
@@ -89,17 +88,13 @@ public class ShapeDescriptorFeatureSetProvider implements
      * Different kinds of shape features
      */
     public enum ShapeDescriptors {
-        CENTRAL_DISTANCE, FD_CENTRAL_DISTANCE, FD_COMPLEX_COORDINATES;
+        CENTROID_DISTANCE, FD_CENTROID_DISTANCE;
     }
 
     private static final int DEFAULT_SAMPLING_RATE = 128;
 
     private static SettingsModelStringArray createFeatModel() {
         return new SettingsModelStringArray("feature_selection", new String[]{});
-    }
-
-    private static SettingsModelInteger createNumBinsModel() {
-        return new SettingsModelInteger("binSize", DEFAULT_SAMPLING_RATE);
     }
 
     private static SettingsModelInteger createSamplingRateModel() {
@@ -114,11 +109,6 @@ public class ShapeDescriptorFeatureSetProvider implements
     /* The kind of descriptor to be calculated */
     private SettingsModelStringArray m_fouriershapeFeat;
 
-    /*
-     * Num of bins
-     */
-    private SettingsModelInteger m_numBins;
-
     /* The sampling rate to calculate the feature */
     private SettingsModelInteger m_samplingRate;
 
@@ -127,32 +117,21 @@ public class ShapeDescriptorFeatureSetProvider implements
                                    final List<DataCell> cells) {
         m_featFac.updateFeatureTarget(roi.a);
 
-        if ((m_samplingRate.getIntValue() % m_numBins.getIntValue()) != 0) {
-            throw new IllegalArgumentException("Wrong configuration: SamplingRate mod bins must be 0");
-        }
-
-        // bins
-        final int binSize = m_samplingRate.getIntValue() / m_numBins.getIntValue();
-
         // Features
         double sum = 0;
         for (int featID = 0; featID < m_featFac.getNumFeatures(); featID++) {
-            sum += m_featFac.getFeatureValue(featID);
-            if ((featID % binSize) == 0) {
-                cells.add(new DoubleCell(sum / binSize));
-                sum = 0.0;
-            }
+            cells.add(new DoubleCell(m_featFac.getFeatureValue(featID)));
         }
     }
 
     @Override
     public String getFeatureSetName() {
-        return "Shape descriptors";
+        return "Shape Descriptors";
     }
 
     @Override
     public String getFeatureSetId() {
-        return "Shape descriptors";
+        return "Shape Descriptors";
     }
 
     @Override
@@ -164,30 +143,22 @@ public class ShapeDescriptorFeatureSetProvider implements
 
         for (int i = 0; i < selectedFeatures.length; i++) {
             switch (ShapeDescriptors.valueOf(selectedFeatures[i])) {
-                case CENTRAL_DISTANCE:
-                    featSets.add(new CentralDistanceFeatureSet(m_samplingRate.getIntValue()));
+                case CENTROID_DISTANCE:
+                    featSets.add(new CentroidDistanceFeatureSet(m_samplingRate.getIntValue()));
                     break;
-                case FD_CENTRAL_DISTANCE:
-                    featSets.add(new FDCentralDistanceFeatureSet(m_samplingRate.getIntValue()));
+                case FD_CENTROID_DISTANCE:
+                    featSets.add(new FDCentroidDistanceFeatureSet(m_samplingRate.getIntValue()));
                     break;
-
-                case FD_COMPLEX_COORDINATES:
-                    featSets.add(new FDComplexCoordinatesFeatureSet(m_samplingRate.getIntValue()));
-                    break;
-
                 default:
                     //
             }
         }
 
-        // bins
-        final int binSize = m_samplingRate.getIntValue() / m_numBins.getIntValue();
-
         m_featFac = new FeatureFactory(true, featSets);
 
         // create outspec according to the selected features
         final String[] featNames = m_featFac.getFeatureNames();
-        for (int i = 0; i < (featNames.length / binSize); i++) {
+        for (int i = 0; i < featNames.length; i++) {
             specs.add(new DataColumnSpecCreator(featNames[i], DoubleCell.TYPE).createSpec());
         }
     }
@@ -195,14 +166,11 @@ public class ShapeDescriptorFeatureSetProvider implements
     @Override
     public void initAndAddDialogComponents(final List<DialogComponent> components) {
 
-        components.add(new DialogComponentStringListSelection(createFeatModel(), "Features", Arrays
+        components.add(new DialogComponentStringListSelection(createFeatModel(), "Shape Descriptors", Arrays
                 .asList(EnumListProvider.getStringList(ShapeDescriptors.values())), true, 2));
 
-        components.add(new DialogComponentNumber(createSamplingRateModel(), "Number of samples (must fit 2^n)", 2));
-
-        components.add(new DialogComponentNumber(createNumBinsModel(),
-                "Number of bins (samplingRate mod bins must be 0)", 1));
-
+        components.add(new DialogComponentNumber(createSamplingRateModel(),
+                "Number of contour points (should fit 2^n, if FD used)", 2));
     }
 
     @Override
@@ -210,7 +178,5 @@ public class ShapeDescriptorFeatureSetProvider implements
         settingsModels.add(m_fouriershapeFeat = createFeatModel());
 
         settingsModels.add(m_samplingRate = createSamplingRateModel());
-
-        settingsModels.add(m_numBins = createNumBinsModel());
     }
 }

org.knime.knip.base/src/org/knime/knip/base/nodes/features/LabFeatureSetNodeFactory.java
@@ -60,7 +60,7 @@ import org.knime.knip.base.nodes.features.providers.ShapeDescriptorFeatureSetPro
 
 /**
  * TODO Auto-generated
- * 
+ *
  * @author <a href="mailto:dietzc85@googlemail.com">Christian Dietz</a>
  * @author <a href="mailto:horn_martin@gmx.de">Martin Horn</a>
  * @author <a href="mailto:michael.zinsmaier@googlemail.com">Michael Zinsmaier</a>

org.knime.knip.base/src/org/knime/knip/base/nodes/features/LabFeatureSetNodeFactory.xml
@@ -18,17 +18,25 @@
 		<tab name="Features">
 			<option name="Selection of the features to be calculated">
 				<ul>
-					<li>Shape features:
-						Absolut distance: The distance from the centroid to
-						the outline of this segment</li>
+					<li>Segment Geometry:
+						Basic geometric properties, such as size,
+						perimeter, ...
+					</li>
 
-					<li>FD features see:
-						Shape Based Image
-						Retrieval Using Generic Fourier Descriptors (Zhang et. al 2004)</li>
-
-					<li>Geometric features:
-						- Segment size, centroid, ...</li>
-
+					<li>Centroid Distance (Shape Descriptors):
+						The distance from the
+						centroid to
+						the outline of this segment.
+					</li>
+					<li>
+						FD Centroid Distance (Shape Descriptors):
+						The distances from the
+						centroid to the contour in fourier space. From the resulting
+						fourier coefficients the magnitude is taken and their are
+						normalized by the first component (cp. Fourier Descriptors).
+						Please note that only (n/2)-1 features will be calculated (where n is the number
+						of contour points).
+					</li>
 				</ul>
 
 			</option>
@@ -37,16 +45,18 @@
 			<option name="Append labels of overlapping ROIs?">
 				If region of interests are overlapping, the union
 				of this labels will
-				be append.          
-            </option>
+				be append.
+			</option>
 			<option name="Depended ROIs don't need to completely overlap?">
 				If selected, regions will also be counted as
 				overlapping, if so don't
-				overlap completely.</option>
+				overlap completely.
+			</option>
 			<option name="Append ROI information">If selected, additional information of each
 				region of interest will be add (e.g. bitmask, source labeling,
 				label). Useful, e.g., if the resulting segment table should be
-				composed again to a labeling (e.g. using the GroupBy-node).</option>
+				composed again to a labeling (e.g. using the GroupBy-node).
+			</option>
 			<option name="Filter labels">
 				If you have multi-labeled pixel positions, this
 				means that a

org.knime.knip.base/src/org/knime/knip/base/nodes/features/providers/SegmentFeatureSetProvider.java
@@ -110,7 +110,7 @@ public class SegmentFeatureSetProvider implements
 
     @Override
     public String getFeatureSetName() {
-        return "Segment geometry";
+        return "Segment Geometry";
     }
 
     @Override

org.knime.knip.core/src/org/knime/knip/core/features/ObjectCalcAndCache.java
@@ -48,6 +48,7 @@
  */
 package org.knime.knip.core.features;
 
+import java.awt.Polygon;
 import java.util.BitSet;
 
 import net.imglib2.Cursor;
@@ -66,11 +67,14 @@ import net.imglib2.type.numeric.RealType;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.knime.knip.core.data.labeling.Signature;
+import org.knime.knip.core.util.PolygonTools;
 
 /**
- * 
- * Utility class which calculates caches commonly used objects.
- * 
+ *
+ * Utility class which calculates "caches" commonly used objects, i.e. as soon as one of the methods is called, the
+ * demanded object is created and returned. On the second call the already created object is returned, if the parameter
+ * object equals (object equality) the parameter object of the previous call.
+ *
  * @author <a href="mailto:dietzc85@googlemail.com">Christian Dietz</a>
  * @author <a href="mailto:horn_martin@gmx.de">Martin Horn</a>
  * @author <a href="mailto:michael.zinsmaier@googlemail.com">Michael Zinsmaier</a>
@@ -236,6 +240,24 @@ public class ObjectCalcAndCache {
         }
 
         return m_signature;
+
+    }
+
+    private IterableInterval<BitType> m_tIterableInterval;
+
+    private Polygon m_polygon;
+
+    private int[] m_offset = new int[2];
+
+    public Polygon traceContour(final IterableInterval<BitType> ii) {
+
+        if (m_sIterableInterval != ii) {
+            m_sIterableInterval = ii;
+
+            m_polygon = PolygonTools.extractPolygon(binaryMask(ii), m_offset);
+
+        }
+        return m_polygon;
 
     }
 

org.knime.knip.core/src/org/knime/knip/core/features/fd/CentroidDistanceFeatureSet.java
@@ -48,65 +48,55 @@
  */
 package org.knime.knip.core.features.fd;
 
+import java.awt.Polygon;
+
 import net.imglib2.IterableInterval;
 import net.imglib2.type.logic.BitType;
 
-import org.knime.knip.core.algorithm.InplaceFFT;
-import org.knime.knip.core.data.algebra.Complex;
-import org.knime.knip.core.data.labeling.Signature;
 import org.knime.knip.core.features.FeatureSet;
 import org.knime.knip.core.features.FeatureTargetListener;
 import org.knime.knip.core.features.ObjectCalcAndCache;
 import org.knime.knip.core.features.SharesObjects;
+import org.knime.knip.core.util.PolygonTools;
 
 /**
- * 
+ * Calculates the distance of the points on the contour to the centroid.
+ *
+ * The contour points are extracted by an contour tracing algorithm (
+ * {@link PolygonTools#extractPolygon(net.imglib2.RandomAccessibleInterval, int[])}).
+ *
  * @author <a href="mailto:dietzc85@googlemail.com">Christian Dietz</a>
  * @author <a href="mailto:horn_martin@gmx.de">Martin Horn</a>
  * @author <a href="mailto:michael.zinsmaier@googlemail.com">Michael Zinsmaier</a>
  */
-public class FDCentralDistanceFeatureSet implements FeatureSet, SharesObjects {
+public class CentroidDistanceFeatureSet implements FeatureSet, SharesObjects {
 
-    /*
-     * Complex array containing the current signature as complex values
-     */
-    private final Complex[] m_complexSignature;
-
-    /*
-     * The Fourier Transformed Signature
-     */
-    private Complex[] m_transformed;
-
-    private final int m_numAngles;
-
-    private Signature m_signature;
-
-    private final double[] m_descriptor;
+    private double[] m_distances;
 
     private ObjectCalcAndCache m_ocac;
 
     /**
-     * @param numAngles
+     * @param numPoints
+     * @param target
      */
-    public FDCentralDistanceFeatureSet(final int numAngles) {
-        m_numAngles = numAngles;
-        m_complexSignature = new Complex[numAngles];
-        m_transformed = new Complex[numAngles];
-        m_descriptor = new double[numAngles];
-
+    public CentroidDistanceFeatureSet(final int numPoints) {
+        m_distances = new double[numPoints];
     }
 
     @FeatureTargetListener
     public void iiUpdated(final IterableInterval<BitType> interval) {
-        m_signature = m_ocac.signature(interval, m_numAngles);
-        for (int x = 0; x < m_signature.length(); x++) {
-            m_complexSignature[x] = new Complex(m_signature.getPosAt(x), 0);
+        Polygon poly = m_ocac.traceContour(interval);
+        final double[] centroid = m_ocac.centroid(interval);
+        for (int i = 0; i < centroid.length; i++) {
+            centroid[i] -= interval.min(i);
         }
+        double tmpx, tmpy;
+        for (int x = 0; x < m_distances.length; x++) {
+            int i = (int)((poly.npoints / (double)m_distances.length) * x);
+            tmpx = poly.xpoints[i] - centroid[0];
+            tmpy = poly.ypoints[i] - centroid[1];
 
-        m_transformed = InplaceFFT.fft(m_complexSignature);
-        final double dcMagnitude = m_transformed[0].getMagnitude();
-        for (int t = 1; t < (m_transformed.length / 2); t++) {
-            m_descriptor[t - 1] = (m_transformed[t].getMagnitude() / dcMagnitude);
+            m_distances[x] = Math.sqrt(tmpx * tmpx + tmpy * tmpy);
         }
     }
 
@@ -115,7 +105,7 @@ public class FDCentralDistanceFeatureSet implements FeatureSet, SharesObjects {
      */
     @Override
     public double value(final int id) {
-        return m_descriptor[id];
+        return m_distances[id];
     }
 
     /**
@@ -123,7 +113,7 @@ public class FDCentralDistanceFeatureSet implements FeatureSet, SharesObjects {
      */
     @Override
     public String name(final int id) {
-        return "FD:CentralDistance [" + id + "]";
+        return "CentroidDistance [" + id + "]";
     }
 
     /**
@@ -131,7 +121,7 @@ public class FDCentralDistanceFeatureSet implements FeatureSet, SharesObjects {
      */
     @Override
     public int numFeatures() {
-        return m_numAngles;
+        return m_distances.length;
     }
 
     /**
@@ -139,7 +129,7 @@ public class FDCentralDistanceFeatureSet implements FeatureSet, SharesObjects {
      */
     @Override
     public String featureSetId() {
-        return "FD Central Distance Feature Factory";
+        return "Centroid Distance Feature Factory";
     }
 
     /**
@@ -147,7 +137,7 @@ public class FDCentralDistanceFeatureSet implements FeatureSet, SharesObjects {
      */
     @Override
     public void enable(final int id) {
-        // nothing to do here
+        // nothing to do
 
     }
 

org.knime.knip.core/src/org/knime/knip/core/features/fd/FDCentroidDistanceFeatureSet.java
@@ -48,40 +48,81 @@
  */
 package org.knime.knip.core.features.fd;
 
+import java.awt.Polygon;
+
 import net.imglib2.IterableInterval;
 import net.imglib2.type.logic.BitType;
 
-import org.knime.knip.core.data.labeling.Signature;
+import org.knime.knip.core.algorithm.InplaceFFT;
+import org.knime.knip.core.data.algebra.Complex;
 import org.knime.knip.core.features.FeatureSet;
 import org.knime.knip.core.features.FeatureTargetListener;
 import org.knime.knip.core.features.ObjectCalcAndCache;
 import org.knime.knip.core.features.SharesObjects;
+import org.knime.knip.core.util.PolygonTools;
 
 /**
- * 
+ *
+ * Calculates the distance of the points on the contour to the centroid and transforms them into the fourier space. The
+ * resulting features are additionally normalized by the first FD-component.
+ *
+ * The contour points are extracted by an contour tracing algorithm (
+ * {@link PolygonTools#extractPolygon(net.imglib2.RandomAccessibleInterval, int[])}).
+ *
  * @author <a href="mailto:dietzc85@googlemail.com">Christian Dietz</a>
  * @author <a href="mailto:horn_martin@gmx.de">Martin Horn</a>
  * @author <a href="mailto:michael.zinsmaier@googlemail.com">Michael Zinsmaier</a>
  */
-public class CentralDistanceFeatureSet implements FeatureSet, SharesObjects {
+public class FDCentroidDistanceFeatureSet implements FeatureSet, SharesObjects {
 
-    private final int m_numAngles;
+    /*
+     * Complex array containing the current signature as complex values
+     */
+    private final Complex[] m_complexSignature;
 
-    private Signature m_signature;
+    /*
+     * The Fourier Transformed Signature
+     */
+    private Complex[] m_transformed;
+
+    private final int m_numContourPoints;
+
+    private final double[] m_descriptor;
 
     private ObjectCalcAndCache m_ocac;
 
     /**
-     * @param numAngles
-     * @param target
+     * @param numContourPoints
      */
-    public CentralDistanceFeatureSet(final int numAngles) {
-        m_numAngles = numAngles;
+    public FDCentroidDistanceFeatureSet(final int numContourPoints) {
+        m_numContourPoints = numContourPoints;
+        m_complexSignature = new Complex[numContourPoints];
+        m_transformed = new Complex[numContourPoints];
+        m_descriptor = new double[(numContourPoints / 2) - 1];
+
     }
 
     @FeatureTargetListener
     public void iiUpdated(final IterableInterval<BitType> interval) {
-        m_signature = m_ocac.signature(interval, m_numAngles);
+        Polygon poly = m_ocac.traceContour(interval);
+        final double[] centroid = m_ocac.centroid(interval);
+        for (int i = 0; i < centroid.length; i++) {
+            centroid[i] -= interval.min(i);
+        }
+        double tmpx, tmpy;
+        for (int x = 0; x < m_numContourPoints; x++) {
+            int i = (int)((poly.npoints / (double)m_numContourPoints) * x);
+            tmpx = poly.xpoints[i] - centroid[0];
+            tmpy = poly.ypoints[i] - centroid[1];
+
+            m_complexSignature[x] = new Complex(Math.sqrt(tmpx * tmpx + tmpy * tmpy), 0);
+        }
+
+        m_transformed = InplaceFFT.fft(m_complexSignature);
+        final double dcMagnitude = m_transformed[0].getMagnitude();
+        for (int t = 1; t < (m_transformed.length / 2); t++) {
+            m_descriptor[t - 1] = (m_transformed[t].getMagnitude() / dcMagnitude);
+        }
     }
 
     /**
@@ -89,7 +130,7 @@ public class CentralDistanceFeatureSet implements FeatureSet, SharesObjects {
      */
     @Override
     public double value(final int id) {
-        return m_signature.getPosAt(id);
+        return m_descriptor[id];
     }
 
     /**
@@ -97,7 +138,7 @@ public class CentralDistanceFeatureSet implements FeatureSet, SharesObjects {
      */
     @Override
     public String name(final int id) {
-        return "Abs:CentralDistance [" + id + "]";
+        return "FD:CentroidDistance [" + id + "]";
     }
 
     /**
@@ -105,7 +146,7 @@ public class CentralDistanceFeatureSet implements FeatureSet, SharesObjects {
      */
     @Override
     public int numFeatures() {
-        return m_numAngles;
+        return m_descriptor.length;
     }
 
     /**
@@ -113,7 +154,7 @@ public class CentralDistanceFeatureSet implements FeatureSet, SharesObjects {
      */
     @Override
     public String featureSetId() {
-        return "Central Distance Feature Factory";
+        return "FD Centroid Distance Feature Factory";
     }
 
     /**
@@ -121,7 +162,7 @@ public class CentralDistanceFeatureSet implements FeatureSet, SharesObjects {
      */
     @Override
     public void enable(final int id) {
-        // nothing to do
+        // nothing to do here
 
     }
 

commit 58c492588bf08d27aaba9e91fc99fa485966e00c

Fixed bug introduced with recent commit 88213ee6ee48c7b114ed1396e4112004e7e5f4e4

org.knime.knip.base/src/org/knime/knip/base/nodes/features/IntervalFeatureSetNodeModel.java
@@ -116,18 +116,18 @@ import org.knime.knip.core.util.MiscViews;
  * A abstract node model, which allows to calculate arbitrary features on {@link IterableInterval}s. That intervals can
  * be either derived from a single image, from a single segments of a labeling or from segments and the according source
  * image toghether (region of interest).
- * 
+ *
  * Subclasses basically only need to specify the type on what the features should be calculated (image, labeling,
  * image_and_labeling) which determines whether to select only an image column, only a labeling column, or an image and
  * a labeling column in the configuration dialog.
- * 
+ *
  * The method {@link IntervalFeatureSetNodeModel#getFeatureSetProviders()}, to be overwritten in the subclasses,
  * determines the available features sets (which provide additional dialog components to configure the features, and
  * actually calculates the feature values).
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * @param <L>
  * @param <T>
  * @author <a href="mailto:dietzc85@googlemail.com">Christian Dietz</a>
@@ -199,7 +199,7 @@ public class IntervalFeatureSetNodeModel<L extends Comparable<L>, T extends Real
     /**
      * @param type the feature type, i.e. which objects are needed to calculate the features: there are either a single
      *            image, image AND labeling or only a labeling
-     * 
+     *
      */
     protected IntervalFeatureSetNodeModel(final FeatureType type,
                                           final FeatureSetProvider<ValuePair<IterableInterval<T>, CalibratedSpace>>[] fsetProviders) {
@@ -315,11 +315,9 @@ public class IntervalFeatureSetNodeModel<L extends Comparable<L>, T extends Real
             final List<DataCell> cells = new ArrayList<DataCell>();
             if (m_type == FeatureType.IMAGE) {
                 img = ((ImgPlusValue<T>)imgCell).getImgPlus();
-                for (final FeatureSetProvider<ValuePair<IterableInterval<T>, CalibratedSpace>> featFacWrapper : m_featSetProviders) {
-                    if (isFeatureSetActive(featFacWrapper.getFeatureSetName())) {
-                        featFacWrapper
-                                .calcAndAddFeatures(new ValuePair<IterableInterval<T>, CalibratedSpace>(img, img),
-                                                    cells);
+                for (final FeatureSetProvider<ValuePair<IterableInterval<T>, CalibratedSpace>> featSet : m_featSetProviders) {
+                    if (isFeatureSetActive(featSet.getFeatureSetId())) {
+                        featSet.calcAndAddFeatures(new ValuePair<IterableInterval<T>, CalibratedSpace>(img, img), cells);
                     }
                 }
                 exec.setProgress(i++ / count, "Calculated features for row " + row.getKey().toString());
@@ -435,9 +433,9 @@ public class IntervalFeatureSetNodeModel<L extends Comparable<L>, T extends Real
                     cells.add(new StringCell(buf.toString()));
                 }
 
-                for (final FeatureSetProvider<ValuePair<IterableInterval<T>, CalibratedSpace>> featFacWrapper : m_featSetProviders) {
-                    if (isFeatureSetActive(featFacWrapper.getFeatureSetName())) {
-                        featFacWrapper
+                for (final FeatureSetProvider<ValuePair<IterableInterval<T>, CalibratedSpace>> featSet : m_featSetProviders) {
+                    if (isFeatureSetActive(featSet.getFeatureSetId())) {
+                        featSet
                                 .calcAndAddFeatures(new ValuePair<IterableInterval<T>, CalibratedSpace>(ii, mdata),
                                                     cells);
                     }
@@ -504,9 +502,9 @@ public class IntervalFeatureSetNodeModel<L extends Comparable<L>, T extends Real
                 specs.add(new DataColumnSpecCreator("LabelDependencies", StringCell.TYPE).createSpec());
             }
         }
-        for (final FeatureSetProvider<ValuePair<IterableInterval<T>, CalibratedSpace>> featFactoryWrapper : m_featSetProviders) {
-            if (isFeatureSetActive(featFactoryWrapper.getFeatureSetName())) {
-                featFactoryWrapper.initAndAddColumnSpecs(specs);
+        for (final FeatureSetProvider<ValuePair<IterableInterval<T>, CalibratedSpace>> featSet : m_featSetProviders) {
+            if (isFeatureSetActive(featSet.getFeatureSetId())) {
+                featSet.initAndAddColumnSpecs(specs);
             }
         }
 
@@ -520,9 +518,9 @@ public class IntervalFeatureSetNodeModel<L extends Comparable<L>, T extends Real
      * @return true, if the specified feature set is marked as active in the
      * dialog
      */
-    private boolean isFeatureSetActive(final String featureSetName) {
+    private boolean isFeatureSetActive(final String featureSetId) {
         for (final String s : m_activeFeatureSets.getStringArrayValue()) {
-            if (featureSetName.equals(s)) {
+            if (featureSetId.equals(s)) {
                 return true;
             }
         }

org.knime.knip.base/src/org/knime/knip/base/nodes/features/providers/ShapeDescriptorFeatureSetProvider.java
@@ -139,7 +139,7 @@ public class ShapeDescriptorFeatureSetProvider implements
         // select the appropriate features
         final String[] selectedFeatures = m_fouriershapeFeat.getStringArrayValue();
 
-        final List<FeatureSet> featSets = new ArrayList<FeatureSet>(3);
+        final List<FeatureSet> featSets = new ArrayList<FeatureSet>(2);
 
         for (int i = 0; i < selectedFeatures.length; i++) {
             switch (ShapeDescriptors.valueOf(selectedFeatures[i])) {