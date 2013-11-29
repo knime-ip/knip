eeb7f1942e06ca5f784523c6dcea616eee1bd2fa
dietzc <christian.dietz@uni-konstanz.de>
2013-11-29 18:17:20 +0100

    Minor Code-Cleanups
    	* Formatting
    	* Deprecation of unused externalizers

--- org.knime.knip.core/src/org/knime/knip/core/io/externalization/externalizers/ImgViewExt0.java
+++ org.knime.knip.core/src/org/knime/knip/core/io/externalization/externalizers/ImgViewExt0.java
@@ -58,7 +58,7 @@ import org.knime.knip.core.io.externalization.ExternalizerManager;
 
 /**
  * Delegates to the naive img externalization.
- * 
+ *
  * @author <a href="mailto:dietzc85@googlemail.com">Christian Dietz</a>
  * @author <a href="mailto:horn_martin@gmx.de">Martin Horn</a>
  * @author <a href="mailto:michael.zinsmaier@googlemail.com">Michael Zinsmaier</a>
