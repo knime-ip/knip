ef414a4b54b2a2e11fde5aa4c18fe16d05401214
dietzc <christian.dietz@uni-konstanz.de>
2014-10-13 17:36:57 +0200

    OverlayAnnotatorView: organize imports

--- org.knime.knip.io/src/org/knime/knip/io/nodes/annotation/create/OverlayAnnotatorView.java
+++ org.knime.knip.io/src/org/knime/knip/io/nodes/annotation/create/OverlayAnnotatorView.java
@@ -48,11 +48,9 @@
  */
 package org.knime.knip.io.nodes.annotation.create;
 
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import javax.swing.JComponent;
 
