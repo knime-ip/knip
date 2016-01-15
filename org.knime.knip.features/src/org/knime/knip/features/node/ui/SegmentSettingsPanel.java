7f7420340d5e9bfd7ffb4258a7cdd2cea4d5bed0
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2016-01-15 20:07:13 +0100

    Extract SegmentSettings.

---
+++ org.knime.knip.features/src/org/knime/knip/features/node/ui/SegmentSettingsPanel.java
@@ -0,0 +1,44 @@
+package org.knime.knip.features.node.ui;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+import org.knime.core.node.defaultnodesettings.DialogComponent;
+import org.knime.knip.features.node.model.SettingsModelFeatureSet;
+
+import net.miginfocom.layout.AC;
+import net.miginfocom.layout.LC;
+import net.miginfocom.swing.MigLayout;
+
+public class SegmentSettingsPanel extends JPanel {
+
+	/**
+	 * serial version uid
+	 */
+	private static final long serialVersionUID = 3993686239750347221L;
+
+	public SegmentSettingsPanel(final DialogComponent appendOverlappingLabels,
+			final DialogComponent intersectionComponent, final DialogComponent appendSegmentInformationComponent) {
+		this.setBorder(BorderFactory.createTitledBorder("Segment Settings"));
+		this.setLayout(new GridBagLayout());
+
+		final GridBagConstraints gbc = SettingsModelFeatureSet.getNewDefaultGridBagConstraints();
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		
+		this.add(appendOverlappingLabels.getComponentPanel(), gbc);
+		
+		gbc.gridy++;
+		
+		this.add(intersectionComponent.getComponentPanel(), gbc);
+
+		gbc.gridy++;
+		
+		this.add(appendSegmentInformationComponent.getComponentPanel(), gbc);
+	}
+}
